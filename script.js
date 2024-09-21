1e66e51f723398e1f901ec91199836c5f22401d6
melanielogan74 <melanielogan74@gmail.com>
2024-09-20 20:30:22 -0400

    Updated script.js for random numbers and display messages stored

--- script.js
+++ script.js
@@ -5,13 +5,36 @@
 //This is the user who will run the program and used to customize message
 const userName = "Carol";
 
-//An array of positive, motivational messages to be used randomly
-const randomMsg = [`Have a nice day, ${userName}!`, `${userName}, anyone who keeps the ability to see beauty never grows old - Franz Kafka`, `You are so much stronger than you think, ${userName}!`, `${userName}, be the reason someone else feels seen, heard, and supported today`];
+//An object containing arrays of positive, motivational messages to randomly generate
+const randomMsgs = {
+    msgLine1: [`Have a nice day, ${userName}!`, `${userName}, anyone who keeps the ability to see beauty never grows old - Franz Kafka`, `You are so much stronger than you think, ${userName}!`, `${userName}, be the reason someone else feels seen, heard, and supported today`],
+    msgLine2: ["Today could be the day!", "You got this!", "Peace, love, and blessings."],
+    msgLine3: ["Go hard or go home!", "Nothing beats a failure but a try.", "Work hard, stay humble."]
+};
 
-//Get a random number that will be used to select a random message from the array
-let randomNum = Math.floor(Math.random() * randomMsg.length);
+//Test to ensure message from an array inside of an object will print
+//console.log(randomMsgs.msgLine1[0]);
 
-//Test that message will print to screen
-console.log(randomMsg[randomNum]);
+//Define variable that will store messages
+const storeMsgs = [];
 
+//Loop through several times to store the combined messages in a single array
+for (let i = 0; i < 10; i++) {
+    
+    //Get random numbers using each property array in the object
+    let randomNum1 = Math.floor(Math.random() * randomMsgs.msgLine1.length);
+    let randomNum2 = Math.floor(Math.random() * randomMsgs.msgLine2.length);
+    let randomNum3 = Math.floor(Math.random() * randomMsgs.msgLine3.length);
+    
+    let test1 = randomMsgs.msgLine1[randomNum1];
+    let test2 = randomMsgs.msgLine2[randomNum2];
+    let test3 = randomMsgs.msgLine3[randomNum3];
+    
+    storeMsgs[i] = `${test1} * ${test2} * ${test3}`;
+    
+}
 
+//Print the combined messages
+for (let j = 0; j < 10; j++) {
+    console.log(storeMsgs[j]);
+}
